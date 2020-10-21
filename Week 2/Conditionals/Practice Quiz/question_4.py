2c462206353edcff5846ece0f68b180b915f4f5e
Sohan Saldanha <65148994+sohanjs111@users.noreply.github.com>
2020-10-21 09:56:16 +0200

    Add files via upload

---
+++ Week 2/Conditionals/Practice Quiz/question_4.py
@@ -0,0 +1,15 @@
+#    Question 4
+#    Is "A dog" smaller or larger than "A mouse"? Is 9999+8888 smaller or larger than 100*100? Replace the plus sign in the
+#    following code to let Python check it for you and then answer.
+
+print("A dog" < "A mouse")
+print(9999+8888 < 100*100)
+
+
+#    Ans:
+#
+#    True
+#    False
+#
+# -----------------
+#
