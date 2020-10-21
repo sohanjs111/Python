0bf7bde63bdbeb46ce23dfdb511fc035688f5528
Sohan Saldanha <65148994+sohanjs111@users.noreply.github.com>
2020-10-21 09:55:23 +0200

    Add files via upload

---
+++ Week 2/Conditionals/Practice Quiz/question_3.py
@@ -0,0 +1,15 @@
+#    Question 3
+#    What’s the output of this code if number equals 10?
+
+number = 10
+
+if number > 11: 
+  print(0)
+elif number != 10:
+  print(1)
+elif number >= 20 or number < 12:
+  print(2)
+else:
+  print(3)
+
+# Ans 2
