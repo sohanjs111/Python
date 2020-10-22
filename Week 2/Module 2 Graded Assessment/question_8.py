0a133221bd92eb511324d5922b67056f74a5d4f4
Sohan Saldanha <65148994+sohanjs111@users.noreply.github.com>
2020-10-22 15:40:39 +0200

    Add files via upload

---
+++ Week 2/Module 2 Graded Assessment/question_8.py
@@ -0,0 +1,8 @@
+#    Question 8
+#    What’s the output of this code?
+
+def sum(x, y):
+		return(x+y)
+print(sum(sum(1,2), sum(3,4)))
+
+# Answer is 10 
---
+++ Week 2/Module 2 Graded Assessment/question_9.py
@@ -0,0 +1,10 @@
+#    Question 9.
+#    What's the value of this Python expression?
+#
+#    ((10 >= 5*2) and (10 <= 5*2))
+
+print((10 >= 5*2) and (10 <= 5*2))
+
+
+# Answer is:
+# True 
