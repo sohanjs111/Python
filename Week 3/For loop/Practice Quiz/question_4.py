ea18d8514bcb1c8e9801efbc91152ab2ff268832
Sohan Saldanha <65148994+sohanjs111@users.noreply.github.com>
2020-10-23 11:40:54 +0200

    Add files via upload

---
+++ Week 3/For loop/Practice Quiz/question_4.py
@@ -0,0 +1,6 @@
+#    Question 4
+#    Write a script that prints the multiples of 7 between 0 and 100. Print one multiple per line and avoid printing any numbers
+#    that aren't multiples of 7. Remember that 0 is also a multiple of 7.
+
+for seven in range(0,100,7):
+    print(seven)
