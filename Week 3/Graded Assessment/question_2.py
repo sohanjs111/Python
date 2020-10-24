453acc6137f74f2262fcebcc64d28f6a32d9e3e9
Sohan Saldanha <65148994+sohanjs111@users.noreply.github.com>
2020-10-24 12:31:19 +0200

    Add files via upload

---
+++ Week 3/Graded Assessment/question_2.py
@@ -0,0 +1,11 @@
+#    Question 2
+#    The show_letters function should print out each letter of a word on a separate line. Fill in the blanks to
+#    make that happen.
+
+
+def show_letters(word):
+	for x in word:
+		print(x)
+
+show_letters("Hello")
+# Should print one line per letter
