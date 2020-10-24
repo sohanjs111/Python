31fe28b33ca619be730cc919989a66e805fa11e4
Sohan Saldanha <65148994+sohanjs111@users.noreply.github.com>
2020-10-24 12:32:01 +0200

    Add files via upload

---
+++ Week 3/Graded Assessment/question_7.py
@@ -0,0 +1,10 @@
+#    Question 7
+#    The following code raises an error when executed. What's the reason for the error?
+
+
+def decade_counter():
+	while year < 50:
+		year += 10
+	return year
+
+# Failure to initialize variables
